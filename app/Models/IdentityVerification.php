f169878848511a27399d0f4ef10cd1e0f1f5a3ad
Ahmed Almabhouh <az540546@gmail.com>
2024-09-24 17:14:01 +0300

    Fix DB Bugs

--- app/Models/IdentityVerification.php
+++ app/Models/IdentityVerification.php
@@ -15,6 +15,6 @@ class IdentityVerification extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_id', id);
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
--- database/migrations/2024_09_24_091505_create_identity_verifications_table.php
+++ database/migrations/2024_09_24_091505_create_identity_verifications_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\IdentityVerification;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +15,7 @@ return new class extends Migration
         Schema::create('identity_verifications', function (Blueprint $table) {
             $table->id();
             $table->string('file');
-            $table->enum('status', []);
+            $table->enum('status', IdentityVerification::STATUS);
 
             $table->foreignId('user_id')->constrained('users', 'id')->cascadeOnDelete();
 
