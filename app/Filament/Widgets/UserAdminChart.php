8625db8e1ee2075b4eceec470ae1975b917e7ddf
areejhsn <skynew.289@gmail.com>
2024-10-05 13:36:47 +0300

    Wedgits For Dashboard

---
+++ app/Filament/Widgets/UserAdminChart.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+
+class UserAdminChart extends ChartWidget
+{
+    protected static ?string $heading = '187.2k';
+    protected static string $color = 'info';
+    protected static ?string $icon = 'heroicon-o-chart-bar';
+    protected static ?string $iconColor = 'info';
+    protected static ?string $iconBackgroundColor = 'info';
+    protected static ?string $label = 'Monthly users chart';
+    protected static ?int $sort = 2;
+    protected static ?string $badge = 'new';
+    protected static ?string $badgeColor = 'success';
+    protected static ?string $badgeIcon = 'heroicon-o-check-circle';
+    protected static ?string $badgeIconPosition = 'after';
+    protected static ?string $badgeSize = 'xs';
+
+
+    public ?string $filter = 'today';
+
+    protected function getFilters(): ?array
+    {
+        return [
+            'today' => 'Today',
+            'week' => 'Last week',
+            'month' => 'Last month',
+            'year' => 'This year',
+        ];
+    }
+    protected function getData(): array
+    {
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Monthly Users chart',
+                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                ],
+            ],
+            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
