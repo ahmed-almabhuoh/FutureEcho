ee522f0ea873e9fa15dff9327e6105cd1f957449
Ahmed Almabhouh <az540546@gmail.com>
2024-11-09 17:36:02 +0200

    Test

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,7 +60,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return auth()->user()->is_admin;
+        return true;
     }
 
     // Relations
