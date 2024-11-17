f74301e4171bfec469996fcd828e7aa7c2993cea
Ahmed Almabhouh <az540546@gmail.com>
2024-11-17 15:29:01 +0200

    Show Memory For Contributors

--- app/Livewire/MemoriesComponent.php
+++ app/Livewire/MemoriesComponent.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Memory;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class MemoriesComponent extends Component
@@ -22,12 +23,32 @@ class MemoriesComponent extends Component
         session()->flash('message', 'Memory deleted successfully!');
     }
 
+    // public function render()
+    // {
+    //     return view('livewire.memories-component', [
+    //         'memories' => Memory::with('capsule', 'user')
+    //             ->where('user_id', auth()->id())
+    //             ->paginate(10),
+    //     ])->title('Future Echo - Memories');
+    // }
+
     public function render()
     {
+        // DB::enableQueryLog();
+        $memories = Memory::withoutGlobalScopes() // Avoid unintentional duplicate conditions
+            ->with(['capsule', 'user'])
+            ->where(function ($query) {
+                $query->where('user_id', auth()->id()) // User is the owner
+                    ->orWhereHas('capsule.contributors', function ($subQuery) {
+                        $subQuery->where('user_id', auth()->id()); // User is a contributor
+                    });
+            })
+            ->paginate(10);
+        // dd(DB::getQueryLog());
+
+
         return view('livewire.memories-component', [
-            'memories' => Memory::with('capsule', 'user')
-                ->where('user_id', auth()->id())
-                ->paginate(10),
+            'memories' => $memories,
         ])->title('Future Echo - Memories');
     }
 }
