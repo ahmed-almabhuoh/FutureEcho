1c716f2353d9709036d8f79508b015a5f8fdd9a5
Ahmed Almabhouh <az540546@gmail.com>
2024-11-19 14:03:26 +0200

    Verified Identities

--- resources/views/livewire/identity-verification-component.blade.php
+++ resources/views/livewire/identity-verification-component.blade.php
@@ -5,30 +5,35 @@
             <x-index index="Management Access" category="Identity" sub-category="Identity Verification"
                 page="Uploading Identity" :category-link="route('identity.verification')" :sub-category-link="route('identity.verification')" :page-link="route('identity.verification')" />
 
-            @if (auth()->user()->identity()->count() == 0)
-                <x-notice description="By verifying your account, you data cannot be lost!" />
-            @endif
-
-            <x-form title="Identity Verification Request" submit-action="submitRequest" cancel-action="cancel"
-                :classes="'col-xl-12'" :is-disabled="is_null($identity)">
-
-                <x-alert />
-
-                <x-file :is-image="true" label="Identity Image" name="identity" :is-required="true" :is-disabled="auth()->user()->identity()->count() != 0" />
-
-                @if (!is_null($identity))
-                    <x-image-preview path="{{ $identity->temporaryUrl() }}" />
+            @if ($uploadedIdentity->status == 'verified')
+                <livewire:identity-verified-component />
+            @else
+                @if (auth()->user()->identity()->count() == 0)
+                    <x-notice description="By verifying your account, you data cannot be lost!" />
                 @endif
 
-                @if (auth()->user()->identity()->count() != 0)
-                    <x-notice
-                        description="You request identity verification before, we will notify you after complete process from our side, thanks for your patient!" />
+                <x-form title="Identity Verification Request" submit-action="submitRequest" cancel-action="cancel"
+                    :classes="'col-xl-12'" :is-disabled="is_null($identity)">
+
+                    <x-alert />
+
+                    <x-file :is-image="true" label="Identity Image" name="identity" :is-required="true"
+                        :is-disabled="auth()->user()->identity()->count() != 0" />
+
+                    @if (!is_null($identity))
+                        <x-image-preview path="{{ $identity->temporaryUrl() }}" />
+                    @endif
+
+                    @if (auth()->user()->identity()->count() != 0)
+                        <x-notice
+                            description="You request identity verification before, we will notify you after complete process from our side, thanks for your patient!" />
+                    @endif
+
+                </x-form>
+
+                @if (!is_null($uploadedIdentity) && $uploadedIdentity->status == 'pending')
+                    <livewire:identity-view-component :uploaded-identity="$uploadedIdentity" />
                 @endif
-
-            </x-form>
-
-            @if (!is_null($uploadedIdentity) && $uploadedIdentity->status == 'pending')
-                <livewire:identity-view-component :uploaded-identity="$uploadedIdentity" />
             @endif
 
         </div>
